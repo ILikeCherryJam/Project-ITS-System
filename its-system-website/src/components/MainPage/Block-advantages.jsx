ec02bc1e3f7bf26cfc28a6d72e527d9d6d7bde0e
ILikeCherryJam <ilikejammuch@gmail.com>
2026-01-07 20:39:20 +0500

    Main Page - done

--- its-system-website/src/components/MainPage/Block-advantages.jsx
+++ its-system-website/src/components/MainPage/Block-advantages.jsx
@@ -10,38 +10,36 @@ function BlockAdvantages() {
                     <Block1Card 
                         img="./src/assets/pictures/quality.svg" 
                         title="ЖЕСТКАЯ ПОЗИЦИЯ ПО КАЧЕСТВУ" 
-                        description=""
-                        description2=""
+                        description="Комплектующих и качеству печати — мы не экономим на комплектующих и тонере, выбираем самые качественные продукты."
+                        description2="Предлагаем лучший результат за оптимальную цену."
                     />
                     <Block1Card 
                         img="./src/assets/pictures/transparency.svg" 
                         title="100 ЧЕСТНОСТИ И ПРОЗРАЧНОСТИ" 
-                        description=""
-                        description2=""
+                        description="У нас не бывает — «недосыпали тонер»."
+                        description2="Наоборот — мы заполним больше, если это предусматривает модель вашего картриджа."
                     />
                     <Block1Card 
                         img="./src/assets/pictures/guarantee.svg" 
                         title="ГАРАНТИЯ" 
-                        description=""
-                        description2=""
+                        description="На все виды работ."
+                        description2="Возникли проблемы после заправки или ремонта? Повторный вызов за наш счет!"
                     />
                     <Block1Card 
                         img="./src/assets/pictures/equipment.svg" 
                         title="СПЕЦИАЛИЗИРОВАННОЕ ОБОРУДОВАНИЕ" 
-                        description=""
-                        description2=""
+                        description="Необходимое для качественной заправки и восстановления картриджей."
                     />
                     <Block1Card 
                         img="./src/assets/pictures/check.svg" 
                         title="ПРОВЕРЯЕМ КАЖДЫЙ КАРТРИДЖ" 
-                        description=""
-                        description2=""
+                        description="На своих принтерах, каждым заправленным картриджем печатаем тестовую страницу."
+                        description2="Это позволяет сократить частоту брака до минимума."
                     />
                     <Block1Card 
                         img="./src/assets/pictures/employers.svg" 
                         title="СИЛЬНЫЙ ШТАТ СОТРУДНИКОВ" 
-                        description=""
-                        description2=""
+                        description="Обученных заправщиков и сертифицированных инженеров со стажем от 5 до 15 лет, которые гарантирую качество."
                     />
                 </div>
             </div>
--- its-system-website/src/components/MainPage/Footer.jsx
+++ its-system-website/src/components/MainPage/Footer.jsx
@@ -1,11 +1,36 @@
 import './css/footer.css';
-import Map from './cards/Map';
 
 function Footer() {
     return (
-        <div className="uk-container block-condition__container">
-            <h1 className="block-title">Как нас найти</h1>
-            <Map />
+        <div className='footer'>
+            <div className="uk-container block-condition__container">
+                <h1 className="block-title">Как нас найти</h1>
+                <div className="footer-box">
+                    <div className="footer__logo hb__first-column">
+                        <div className="hb__logo">
+                          <img src="./src/assets/logo/test-logo.png" alt="test-logo" />
+                        </div>
+                        <p className="hb__slogan">
+                            Ремонт, заправка и обслуживание оргтехники
+                        </p>
+                    </div>
+                    <div className="footer-info">
+                        <div className="footer__phone-number foot-margin">
+                            <p>+х (ххх) ххх-хх-хх</p>
+                            <p>+х (ххх) ххх-хх-хх</p>
+                        </div>
+                        <p className='footer-adress foot-margin'>
+                            Казахстан, город Астана, район Байконыр, улица Ахмета Жубанова, дом 4
+                            </p>
+                        <a className='footer-link foot-margin' href="#">insert the link</a>
+                        <div className="header-contacts foot-contacts foot-margin">
+                            <img src="./src/assets/contacts/telegram.svg" alt="svg" />
+                            <img src="./src/assets/contacts/whatsapp.svg" alt="svg" />
+                            <img src="./src/assets/contacts/phone.svg" alt="svg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
